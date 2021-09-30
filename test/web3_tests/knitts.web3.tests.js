be002a67be731b5ce95b7cfcb39ff72bcc408e4f
Arvinth_s <arvinth.saravana@gmail.com>
2021-09-30 07:27:21 +0530

    completed mocha tests

--- test/web3_tests/knitts.web3.tests.js
+++ test/web3_tests/knitts.web3.tests.js
@@ -70,104 +70,107 @@ describe('Knitts', async function(){
 });
 
 
-// describe('Knitts-League', async()=>{
-//     var knitts;
-//     var accounts;
-//     var league;
-//     var description;
-//     var moderator;
-//     var randomAccount;
-//     var participants;
-//     var investors;
-//     var user;
-//     before(async function(){
-//         accounts = await web3.eth.getAccounts();
-//         organization = moderator = accounts[0];
-//         randomAccount = accounts[8];
-//         participants = [accounts[1], accounts[2]];
-//         investors = [accounts[3], accounts[4], accounts[5]];
-//         knitts = await Knitts.new({from:organization});
-//         for(let i=0; i<4 ; i++){
-//             await knitts.register.sendTransaction("Arvinth", { from: accounts[i]});
-//         }
-//         sentence = ["OM", "NAMO", "NARAYANA"];
-//         description = convert2Bytes(sentence, 20);
-//     });
-//     it('should allow moderator to create a league', async()=>{
-//         await knitts.addModerator.sendTransaction({from: moderator, value:web3.utils.toWei('1', 'ether')});
-//         await knitts.addModerator.call({from: moderator, value:web3.utils.toWei('1', 'ether')});
-//         await knitts.createLeague.sendTransaction(web3.utils.toWei('0.1', 'ether'), 3, 1000, {from:moderator});
-    
-//         var leagueAddress = await knitts.createLeague.call(web3.utils.toWei('0.1', 'ether'), 3, 1000, {from:moderator});
-//         league = await League.at(leagueAddress[0]);
-//         var league_details = await league.getDetails.call();
-//         assert(league_details[0] == moderator, "moderator address is different");
-//     });
-//     it('should allow participants to enter', async()=>{
-//         await league.submitIdea.sendTransaction(description, {from: participants[0], value: web3.utils.toWei('0.1', 'ether')});
-//         await league.submitIdea.sendTransaction(description, {from: participants[1], value: web3.utils.toWei('0.1', 'ether')});
-//         var numParticipants = await league.submitIdea.call(description, {from: participants[0], value: web3.utils.toWei('0.1', 'ether')});
-//         assert(numParticipants > 0, 'participants not updated properly');
-//         console.log('# of participants:', numParticipants);
-//     });
+describe('Knitts-League', async()=>{
+    var knitts;
+    var accounts;
+    var league;
+    var description;
+    var moderator;
+    var randomAccount;
+    var participants;
+    var investors;
+    var user;
+    before(async function(){
+        accounts = await web3.eth.getAccounts();
+        organization = moderator = accounts[0];
+        randomAccount = accounts[8];
+        participants = [accounts[1], accounts[2]];
+        investors = [accounts[3], accounts[4], accounts[5]];
+        knitts = await new web3.eth.Contract(Knitts.abi, {from:organization});
+        knitts = await knitts.deploy({data:Knitts.bytecode}).send({from:organization, gas:1e7});
+        for(let i=0; i<3 ; i++){
+            await knitts.methods.register("Arvinth").send({ from: accounts[i], gas:1e7});
+        }
+        sentence = ["OM", "NAMO", "NARAYANA"];
+        description = convert2Bytes(sentence, 20);
+    });
+    it('should allow moderator to create a league', async()=>{
+        await knitts.methods.addModerator().send({from: moderator, value:web3.utils.toWei('1', 'ether'), gas:1e7});
+        await knitts.methods.createLeague(web3.utils.toWei('0.1', 'ether'), 3, 1000).send( {from:moderator , gas:1e7});
+        var leagueAddress = await knitts.methods.createLeague(web3.utils.toWei('0.1', 'ether'), 3, 1000).call( {from:moderator , gas:1e7});
 
-//     it('allows investor to invest', async()=>{
-//         await league.invest.sendTransaction(0, {from:investors[0], value: web3.utils.toWei('1', 'ether')});
-//         await league.invest.sendTransaction(1, {from:investors[1], value: web3.utils.toWei('2', 'ether')});
-//         await league.invest.sendTransaction(0, {from: investors[2], value:web3.utils.toWei('1', 'ether')});
-//     });
+        // console.log('leagueaddress: ', leagueAddress);
+        // var leagueAddress = await knitts.methods.createLeague(web3.utils.toWei('0.1', 'ether'), 3, 1000).call( {from:moderator , gas:1e7});
+        // console.log('leagueaddress: ', leagueAddress);
+        league = await new web3.eth.Contract(League.abi, leagueAddress[0]);
+        var league_details = await league.methods.getDetails().call();
+        assert(league_details[0] == moderator, "moderator address is different");
+    });
+    it('should allow participants to enter', async()=>{
+        await league.methods.submitIdea(description).send({from: participants[0], value: web3.utils.toWei('0.1', 'ether'), gas:1e7});
+        await league.methods.submitIdea(description).send({from: participants[1], value: web3.utils.toWei('0.1', 'ether'), gas:1e7});
+        var numParticipants = await league.methods.submitIdea(description).call({from: participants[0], value: web3.utils.toWei('0.1', 'ether'), gas:1e7});
+        assert(numParticipants > 0, 'participants not updated properly');
+        console.log('# of participants:', numParticipants);
+    }).timeout(20000);
 
-//     it('should return the points', async()=>{
-//         // await league.submitIdea.sendTransaction(description, {from: participants[1], value: web3.utils.toWei('0.1', 'ether')});
-//         points = await league.endLeague.sendTransaction({from: organization});
-//         points = await league.endLeague.call({from: organization});
-//         console.log('points:', points);
-//     });
+    it('allows investor to invest', async()=>{
+        await league.methods.invest(0).send({from:investors[0], value: web3.utils.toWei('1', 'ether'), gas:1e7});
+        await league.methods.invest(1).send({from:investors[1], value: web3.utils.toWei('2', 'ether'), gas:1e7});
+        await league.methods.invest(0).send({from: investors[2], value:web3.utils.toWei('1', 'ether'), gas:1e7});
+    }).timeout(20000);
 
-//     it('should distribute the amount', async()=>{
-//         var init_balance = [
-//                         await web3.eth.getBalance(accounts[0]),
-//                         await web3.eth.getBalance(accounts[1]),
-//                         await web3.eth.getBalance(accounts[2]),
-//                         await web3.eth.getBalance(accounts[3]),
-//                         await web3.eth.getBalance(accounts[4]),
-//                         await web3.eth.getBalance(accounts[5]),
-//                         await web3.eth.getBalance(accounts[6]),
-//                         await web3.eth.getBalance(accounts[7]),
-//                         await web3.eth.getBalance(accounts[8]),
-//                         await web3.eth.getBalance(accounts[9]), 
-//                         ];
-//         await league.distribute.sendTransaction({from: organization});
+    it('should return the points', async()=>{
+        // await league.submitIdea.sendTransaction(description, {from: participants[1], value: web3.utils.toWei('0.1', 'ether')});
+        points = await league.methods.endLeague().send({from: organization, gas:1e7});
+        points = await league.methods.endLeague().call({from: organization});
+        console.log('points:', points);
+    }).timeout(20000);
 
-//         var final_balance = [
-//             await web3.eth.getBalance(accounts[0]),
-//             await web3.eth.getBalance(accounts[1]),
-//             await web3.eth.getBalance(accounts[2]),
-//             await web3.eth.getBalance(accounts[3]),
-//             await web3.eth.getBalance(accounts[4]),
-//             await web3.eth.getBalance(accounts[5]),
-//             await web3.eth.getBalance(accounts[6]),
-//             await web3.eth.getBalance(accounts[7]),
-//             await web3.eth.getBalance(accounts[8]),
-//             await web3.eth.getBalance(accounts[9]), 
-//             ];
+    it('should distribute the amount', async()=>{
+        var init_balance = [
+                        await web3.eth.getBalance(accounts[0]),
+                        await web3.eth.getBalance(accounts[1]),
+                        await web3.eth.getBalance(accounts[2]),
+                        await web3.eth.getBalance(accounts[3]),
+                        await web3.eth.getBalance(accounts[4]),
+                        await web3.eth.getBalance(accounts[5]),
+                        await web3.eth.getBalance(accounts[6]),
+                        await web3.eth.getBalance(accounts[7]),
+                        await web3.eth.getBalance(accounts[8]),
+                        await web3.eth.getBalance(accounts[9]), 
+                        ];
+        await league.methods.distribute().send({from: organization, gas:1e7});
 
-//         console.log('init balance: ', init_balance);
-//         console.log('final balance: ', final_balance);
-//     })
-//     it('Returns the details of the users & their projects', async () => {
-//         let userConractAddress = await knitts.getUserContractAddress.call( participants[0] , { from:organization})
-//         user = await User.at(userConractAddress);
-//         let projectDetails = await user.getDetails.call();
-//         console.log(projectDetails);
-//     });
-//     it('Returns the details of the users & their projects - 2', async () => {
-//         let userConractAddress = await knitts.getUserContractAddress.call( participants[1] , { from:organization})
-//         user = await User.at(userConractAddress);
-//         let projectDetails = await user.getDetails.call();
-//         console.log(projectDetails);
-//     });
-// });
+        var final_balance = [
+            await web3.eth.getBalance(accounts[0]),
+            await web3.eth.getBalance(accounts[1]),
+            await web3.eth.getBalance(accounts[2]),
+            await web3.eth.getBalance(accounts[3]),
+            await web3.eth.getBalance(accounts[4]),
+            await web3.eth.getBalance(accounts[5]),
+            await web3.eth.getBalance(accounts[6]),
+            await web3.eth.getBalance(accounts[7]),
+            await web3.eth.getBalance(accounts[8]),
+            await web3.eth.getBalance(accounts[9]), 
+            ];
+
+        console.log('init balance: ', init_balance);
+        console.log('final balance: ', final_balance);
+    }).timeout(20000);
+    it('Returns the details of the users & their projects', async () => {
+        let userConractAddress = await knitts.methods.getUserContractAddress(participants[0]).call({ from:organization})
+        user = await new web3.eth.Contract(User.abi, userConractAddress);
+        let projectDetails = await user.methods.getDetails().call();
+        console.log(projectDetails);
+    }).timeout(20000);
+    it('Returns the details of the users & their projects - 2', async () => {
+        let userConractAddress = await knitts.methods.getUserContractAddress( participants[1]).call({ from:organization})
+        user = await new web3.eth.Contract( User.abi, userConractAddress);
+        let projectDetails = await user.methods.getDetails().call();
+        console.log(projectDetails);
+    }).timeout(20000);
+});
 
 
 
